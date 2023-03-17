5ebef817bb8bf809e60800b71dfff3cfb91314fd
fadelimfa <fadeli.mfa@gmail.com>
2023-03-17 15:16:56 -0700

    er

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - it prints if a number is + or -
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		printf("%i\n", c);
+		c++;
+	}
+
+	return (0);
+}
