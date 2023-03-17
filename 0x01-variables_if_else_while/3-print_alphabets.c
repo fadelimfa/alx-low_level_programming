b0d74e8f4d5b13a90812cb4743f038173891f521
fadelimfa <fadeli.mfa@gmail.com>
2023-03-17 14:51:00 -0700

    s

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - it prints if a number is + or -
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
