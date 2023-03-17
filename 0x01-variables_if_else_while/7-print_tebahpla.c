ce2de051c169afbe8c6f91881b45bb30f063e2ff
fadelimfa <fadeli.mfa@gmail.com>
2023-03-17 15:33:55 -0700

    sfd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - it prints if a number is + or -
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; --c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
