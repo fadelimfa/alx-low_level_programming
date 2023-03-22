c44565704114d971ea2ddbc18adf1bdb3d729d46
fadelimfa <fadeli.mfa@gmail.com>
2023-03-22 05:00:24 -0700

    ds

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - it prints if a number is + or -
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
