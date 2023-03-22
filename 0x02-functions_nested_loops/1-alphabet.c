e8e7110df91f56cb39984ac8dc9a18c84e4a44cb
fadelimfa <fadeli.mfa@gmail.com>
2023-03-22 06:38:38 -0700

    ds

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - it prints if a number is + or -
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+#include "main.h"
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,14 @@
 
 int main(void);
 
+void print_alphabet(void){
+	char letter = 'a';
+	while (letter <= 'z') {
+		 putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
+
+
 #endif /* MAIN_H */
