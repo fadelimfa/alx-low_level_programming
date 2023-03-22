483a10319076f8bf2c0ce8aabe02727d2bc7e9dd
fadelimfa <fadeli.mfa@gmail.com>
2023-03-22 11:20:58 -0700

    sd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_sign - Prints if alphabet or not
+ * @n: is the variable
+ * Return: 0 or 1
+ */
+int _abs(int ab)
+{
+	double a = abs(ab);
+	printf("%a",a);
+}
+
