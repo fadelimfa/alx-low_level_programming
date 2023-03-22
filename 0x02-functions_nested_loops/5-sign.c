dc81fe462a3d273436a8537edb47ed5216eb2c15
fadelimfa <fadeli.mfa@gmail.com>
2023-03-22 10:51:53 -0700

    sd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		printf("+");
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		printf("-");
+		return (-1);
 	}else
 	{
-		return (0);
 		printf("0");
+		return (0);
 	}
 }
 
