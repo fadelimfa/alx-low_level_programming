91006207fd668400d0250b87ce851617049a6c14
fadelimfa <fadeli.mfa@gmail.com>
2023-03-22 10:47:39 -0700

    sd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isalpha - Prints if alphabet or not
+ * @c: is the variable
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (i > 0)
+	{
+		return (1);
+		printf("\+");
+	}
+	else if (i < 0)
+	{
+		return (-1);
+		printf("\-");
+	}else
+	{
+		return (0);
+		printf("0");
+	}
+}
+
