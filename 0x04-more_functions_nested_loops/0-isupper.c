2428cca20b248447c41b4a48f8752eda751f0408
fadelimfa <fadeli.mfa@gmail.com>
2023-03-23 08:18:13 -0700

    d

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int a = isupper(c);
+	if (a>0)
+	{
+		return (1);
+	}else
+	{
+		return (0);
+	}
+}
