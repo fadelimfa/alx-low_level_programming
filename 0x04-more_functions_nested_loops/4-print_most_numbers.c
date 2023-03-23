471d2fcb550fb3edbea665f699806c8799ad7c41
fadelimfa <fadeli.mfa@gmail.com>
2023-03-23 10:47:25 -0700

    sd

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_most_numbers - check the code.
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		if (n = 2)
+			break;
+		else if (n = 4)
+			break;
+		else
+		_putchar((n % 10) + '0');
+
+	_putchar('\n');
+}
