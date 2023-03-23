7901a7af8f0833e3ef974102149af2d6aba7e90a
fadelimfa <fadeli.mfa@gmail.com>
2023-03-23 08:31:20 -0700

    ds

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mul - check the code.
+ *@a: is
+ *@b: is
+ * Return: Always 0.
+ */
+int mul(int a, int b);
+{
+	printf(a*b);
+}
