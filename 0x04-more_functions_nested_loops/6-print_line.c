17ce693c6aeee3f8ba5c0511a52b835854dadeed
fadelimfa <fadeli.mfa@gmail.com>
2023-03-23 14:42:47 -0700

    h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_line - check the code.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n<=0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('/n');
+	}
+}
