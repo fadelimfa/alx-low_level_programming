021dc4c2f8b9636a43125bc93011f55476e3c642
fadelimfa <fadeli.mfa@gmail.com>
2023-03-23 10:44:07 -0700

    sw

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * mul - check the code.
- *@a: is
- *@b: is
+ * print_numbers - check the code.
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int n;
-	for (n = 0; n <=9;n++)
+
+	for (n = 0; n <= 9; n++)
 		_putchar((n % 10) + '0');
-	
+
 	_putchar('\n');
 }
