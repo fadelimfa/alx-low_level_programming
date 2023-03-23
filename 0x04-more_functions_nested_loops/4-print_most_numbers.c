153b0bbb0c568ee487c57e124e089435b178e977
fadelimfa <fadeli.mfa@gmail.com>
2023-03-23 10:48:36 -0700

    sd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,13 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
+	{
 		if (n = 2)
 			break;
 		else if (n = 4)
 			break;
 		else
 		_putchar((n % 10) + '0');
-
+	}
 	_putchar('\n');
 }
