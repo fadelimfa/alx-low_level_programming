ef32f3f74878d2d24c82864f0dc059ec04c199db
fadelimfa <fadeli.mfa@gmail.com>
2023-03-28 09:10:35 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - check the code.
+ *@str: is
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	printf(%p,strrev(s));
+}
