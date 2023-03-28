cee4bc6c548c823676f2aabc36b3ad2ee4a32de9
fadelimfa <fadeli.mfa@gmail.com>
2023-03-28 07:38:32 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * swap_int - check the code.
+ *@a: is
+ *@b: is
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b);
+{
+	*a = &b;
+	*b = &a;
+}
