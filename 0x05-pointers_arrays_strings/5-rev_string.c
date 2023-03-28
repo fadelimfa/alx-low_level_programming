33d2bafcf54b6178f405250bf4ee15aa6bb150b8
fadelimfa <fadeli.mfa@gmail.com>
2023-03-28 09:35:50 -0700

    d

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * rev_string - check the code.
+ *@s: is
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+    int i, j;
+    char temp;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
