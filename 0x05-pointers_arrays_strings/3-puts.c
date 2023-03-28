dff6b7818cadb569a335af2935e747d3c7b85bc6
fadelimfa <fadeli.mfa@gmail.com>
2023-03-28 09:05:44 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * puts - check the code.
+ *@s: is
+ * Return: Always 0.
+ */
+void _puts(char *str);
+{
+	puts(str);
+}
