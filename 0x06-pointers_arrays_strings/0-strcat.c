03e523f8bf59ef0a91e2d5392975886e276a25f5
fadelimfa <fadeli.mfa@gmail.com>
2023-03-29 06:12:25 -0700

    sd

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * *_strcat - check the code.
+ *@dest: is
+ *@src: is
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+}
