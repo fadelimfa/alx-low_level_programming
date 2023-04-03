4718a0e7781521c7f9f76ac9f9348ab742074874
fadelimfa <fadeli.mfa@gmail.com>
2023-04-03 12:10:34 -0700

    ds

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * puts_half - check the code.
+ *@str: is
+ * Return: Always 0.
+ */
+void puts_half(char *str);
+{
+	int len = strlen(str);
+	int i = (len - 1) / 2;
+
+	printf("%s\n", &str[i]);
+
+}
