d1a3033e18d1481d925c21ea39deffd13bc49ef4
fadelimfa <fadeli.mfa@gmail.com>
2023-04-04 07:32:32 -0700

    sd

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,25 @@
-#include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
-
+#include "main.h"
 /**
  * print_line - check the code.
- *@n: is
  * Return: Always 0.
  */
+
 void print_line(int n)
 {
-	int i = 0;
 
-		if (n <= 0)
-		{
+	if(n <= 0 ){
 			putchar('\n');
-		}
-		else
-		{
-			while (i <= n)
-			{
-				putchar('_');
-				i++;
-			}
+			return;
 	}
+	for (int i = 0; i < n; i++){
+		putchar('_');
+	}
+	putchar('\n');
+
 }
+
+
+
+
