dba78e328db49b5b2ec0f7aa1bd6a50a62a475b5
fadelimfa <fadeli.mfa@gmail.com>
2023-04-06 06:00:11 -0700

    ds

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,18 +9,18 @@
  */
 int _sqrt_recursion(int n)
 {
+	int i;
+
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	
-	int i = 1;
+	for (i = 1; i * i <= n; i++)
+	;
 
-	while (i * i < n)
-		i++;
+	i--;
 
 	if (i * i == n)
 		return (i);
-	else
-		return (-1);
+	return (-1);
 }
