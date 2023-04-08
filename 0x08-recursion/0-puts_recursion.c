d717292d1eba7f43585cb7b76c8de1fd544dab0b
fadelimfa <fadeli.mfa@gmail.com>
2023-04-08 06:33:55 -0700

    ds

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[-1] == '\0')
+	if (*s == '\0')
 		putchar('\n');
 	else
 	{
-		putchar(s[-1]);
-		_puts_recursion(s - 1);
+		putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
