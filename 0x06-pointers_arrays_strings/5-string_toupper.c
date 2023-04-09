193703642ff59f967f6f2e4e4f95311cbc6823fe
fadelimfa <fadeli.mfa@gmail.com>
2023-04-09 06:53:19 -0700

    ds

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * *_strcat - check the code.
+ *@dest: is
+ *@src: is
+ * Return: Always 0.
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		s[i] = toupper(s[i]);
+		i++;
+	}
+	return (s);
+
+}
