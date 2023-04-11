e55bb9d42ba1596f95ea91c81898b89d5cf52cc2
fadelimfa <fadeli.mfa@gmail.com>
2023-04-11 04:24:00 -0700

    ds

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,26 +12,26 @@
 
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        {
-            sign *= -1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
-        }
-        else if (result > 0)
-        {
-            break;
-        }
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+	else if (s[i] >= '0' && s[i] <= '9')
+		{
+		result = result * 10 + (s[i] - '0');
+		}
+	else if (result > 0)
+		{
+		break;
+		}
+	i++;
+	}
 
-    return result * sign;
+	return (result * sign);
 }
