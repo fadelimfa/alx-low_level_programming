0a9740ded5a726b58b40b650017fcfa701fc6496
fadelimfa <fadeli.mfa@gmail.com>
2023-04-11 01:20:33 -0700

    sd

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ *_atoi - check the code.
+ *@s: is
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	char str[] = "1234";
+	int num = atoi(str);
+
+	printf("num = %d\n", num);
+	return (0);
+
+}
