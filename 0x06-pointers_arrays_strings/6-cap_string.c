1035a56ef9f13c26edbd08179e7f95d39378d304
fadelimfa <fadeli.mfa@gmail.com>
2023-04-13 10:35:44 -0700

    ds

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * *_strcat - check the code.
+ *@dest: is
+ *@src: is
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+
+
+}
