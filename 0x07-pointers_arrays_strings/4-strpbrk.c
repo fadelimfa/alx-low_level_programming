e159aad019dafa7c65dfb3b57bcf6a0ec9aae73e
fadelimfa <fadeli.mfa@gmail.com>
2023-04-13 10:11:57 -0700

    sd

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ *_strpbrk - check the code.
+ *@s: is
+ *@accept: is
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
