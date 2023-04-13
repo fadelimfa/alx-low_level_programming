2affa43c38713f685fabffc3b5a01e4fd810657a
fadelimfa <fadeli.mfa@gmail.com>
2023-04-13 10:14:43 -0700

    sd

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ *_strstr - check the code.
+ *@haystack: is
+ *@needle: is
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
