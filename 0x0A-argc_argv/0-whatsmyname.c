ebf0f9b8b39ba0a5e986d32052a963bee8e84647
fadelimfa <fadeli.mfa@gmail.com>
2023-04-15 01:14:24 -0700

    sd

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *@argv: is
+ * Return: Always 0.
+ */
+int main(char *argv[])
+{
+	printf("%s", argv[0]);
+	
+	Return(0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int main(char *argv[]);
+#endif /* MAIN_H */
