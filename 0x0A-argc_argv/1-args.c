617e65cf5dc704d647c78ca4f2446db3893324c7
fadelimfa <fadeli.mfa@gmail.com>
2023-04-15 02:43:20 -0700

    ds

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point for the program
+ * @argc: number of command-line arguments
+ * @argv: array of command-line argument strings
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv[];
+
+	return (0);
+}
