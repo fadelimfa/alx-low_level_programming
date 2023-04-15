3d2979336901645aa3e5e855abfe1f6347f44fa5
fadelimfa <fadeli.mfa@gmail.com>
2023-04-15 05:12:18 -0700

    ds

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point for the program
+ * @argc: number of command-line arguments
+ * @argv: array of command-line argument strings
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 0)
+	{
+		printf("Error");
+		return (0);
+	}
+	else
+	{
+		int i = (*argv[1] - '0') * (*argv[2] - '0');
+
+		printf("%d\n", i);
+		return (0);
+	}
+}
