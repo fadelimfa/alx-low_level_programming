0362c4f2dc91e72e38e05074fde020a25d1a61d2
fadelimfa <fadeli.mfa@gmail.com>
2023-04-16 06:19:07 -0700

    sd

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,27 +4,27 @@
 #include <string.h>
 
 /**
- * cap_string - check the code.
+ * leet - check the code.
  *@s: is
  * Return: Always 0.
  */
 char *leet(char *s)
 {
-    int i, j;
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "4433007711";
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (s[i] == letters[j])
-            {
-                s[i] = numbers[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
 
-    return s;
+	return (s);
 }
