6d1f9bf79cd2f2cef4aecf742697f92d31fac13c
fadelimfa <fadeli.mfa@gmail.com>
2023-04-16 06:24:58 -0700

    sd

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_strdup - check the code.
+ *@str: is
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	return (strdup(str));	
+}
