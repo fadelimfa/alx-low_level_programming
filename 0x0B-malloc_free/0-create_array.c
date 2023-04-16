b01e66eae13231ec1ba3d04426022f6958f5fdba
fadelimfa <fadeli.mfa@gmail.com>
2023-04-16 03:13:58 -0700

    sa

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,19 +10,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	    if (size == 0) {
+    char *array;
+    unsigned int i;
+
+    if (size == 0)
         return NULL;
-    }
 
-    char *array = malloc(size * sizeof(char));
-
-    if (array == NULL) {
+    array = malloc(size * sizeof(char));
+    if (array == NULL)
         return NULL;
-    }
 
-    for (unsigned int i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
         array[i] = c;
-    }
 
     return array;
 }
