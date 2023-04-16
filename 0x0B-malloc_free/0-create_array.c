f38a358e3024540840af89c608e6c7afd1ec1824
fadelimfa <fadeli.mfa@gmail.com>
2023-04-16 03:09:49 -0700

    sa

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * reset_to_98 - check the code.
+ *@n: is
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	    if (size == 0) {
+        return NULL;
+    }
+
+    char *array = malloc(size * sizeof(char));
+
+    if (array == NULL) {
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < size; i++) {
+        array[i] = c;
+    }
+
+    return array;
+}
