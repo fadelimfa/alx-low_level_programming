31bc50ae485ea4e28c9187f0b1849dc704059418
fadelimfa <fadeli.mfa@gmail.com>
2023-04-18 10:16:16 -0700

    ds

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,23 +12,23 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int len1, len2;
+	char *concat;
+	int len1, len2;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-    concat = malloc(len1 + len2 + 1);
-    if (concat == NULL)
-        return (NULL);
+	concat = malloc(len1 + len2 + 1);
+	if (concat == NULL)
+		return (NULL);
 
-    strcpy(concat, s1);
-    strcat(concat, s2);
+	strcpy(concat, s1);
+	strcat(concat, s2);
 
-    return (concat);
+	return (concat);
 }
