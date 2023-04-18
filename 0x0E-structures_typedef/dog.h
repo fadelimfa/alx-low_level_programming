58e7f571cea8375810c888d32054676fba5fff76
fadelimfa <fadeli.mfa@gmail.com>
2023-04-18 08:51:52 -0700

    ds

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - check the code.
+ *@name: is
+ *@age: is
+ *@owner: is
+ * Return: Always 0.
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char *owner;	
+	char *owner;
 };
 
 #endif
