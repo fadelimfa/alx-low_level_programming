8dc80418875580f9994a8c2afe61b413dd8a05a3
fadelimfa <fadeli.mfa@gmail.com>
2023-04-18 10:15:08 -0700

    ds

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *str_concat - check the code.
+ *@s1: is
+ *@s2: is
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+    char *concat;
+    int len1, len2;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    len1 = strlen(s1);
+    len2 = strlen(s2);
+
+    concat = malloc(len1 + len2 + 1);
+    if (concat == NULL)
+        return (NULL);
+
+    strcpy(concat, s1);
+    strcat(concat, s2);
+
+    return (concat);
+}
