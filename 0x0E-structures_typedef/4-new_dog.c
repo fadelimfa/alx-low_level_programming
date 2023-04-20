5a578de2de4f0b53d39249bf2f48804263425e65
fadelimfa <fadeli.mfa@gmail.com>
2023-04-20 08:42:51 -0700

    ds

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,40 +2,60 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
 /**
- *print_dog - check the code.
- *@d: is
- * Return: Always 0.
+ * new_dog - creates a new dog
+ * @name: name of the dog (will be copied)
+ * @age: age of the dog
+ * @owner: owner of the dog (will be copied)
+ *
+ * Return: a pointer to the new dog, or NULL if malloc fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
+    /* allocate memory for the new dog */
+    dog_t *new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return NULL;
 
-    /* allocate memory for the dog */
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
-
-    /* allocate memory for the name and owner strings */
-    dog->name = malloc(strlen(name) + 1);
-    if (dog->name == NULL) {
-        free(dog);
-        return (NULL);
+    /* copy the name and owner */
+    int name_len = _strlen(name);
+    new_dog->name = malloc(sizeof(char) * (name_len + 1));
+    if (new_dog->name == NULL) {
+        free(new_dog);
+        return NULL;
     }
-    dog->owner = malloc(strlen(owner) + 1);
-    if (dog->owner == NULL) {
-        free(dog->name);
-        free(dog);
-        return (NULL);
+    for (int i = 0; i <= name_len; i++)
+        new_dog->name[i] = name[i];
+
+    int owner_len = _strlen(owner);
+    new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+    if (new_dog->owner == NULL) {
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
     }
+    for (int i = 0; i <= owner_len; i++)
+        new_dog->owner[i] = owner[i];
 
-    /* copy the name and owner strings */
-    strcpy(dog->name, name);
-    strcpy(dog->owner, owner);
+    /* initialize the new dog */
+    new_dog->age = age;
 
-    /* set the age */
-    dog->age = age;
+    /* return the new dog */
+    return new_dog;
+}
 
-    return (dog);
+/* helper function */
+
+/**
+ * _strlen - computes the length of a string
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+    while (s[len] != '\0')
+        len++;
+    return len;
 }
