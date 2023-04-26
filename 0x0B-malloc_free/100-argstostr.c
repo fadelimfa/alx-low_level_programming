5516362d1214093d91e5a36d5847988a42943f38
fadelimfa <fadeli.mfa@gmail.com>
2023-04-26 08:36:51 -0700

    ds

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,36 +11,36 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *concat_str;
-    int i, j, k = 0, len = 0;
+	char *concat_str;
+	int i, j, k = 0, len = 0;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    /* calculate length of the concatenated string */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++; /* for the newline character */
-    }
+	/* calculate length of the concatenated string */
+	for (i = 0; i < ac; i++)
+	{
+	for (j = 0; av[i][j]; j++)
+		len++;
+	len++; /* for the newline character */
+	}
 
-    concat_str = malloc((len + 1) * sizeof(char));
-    if (concat_str == NULL)
-        return (NULL);
+	concat_str = malloc((len + 1) * sizeof(char));
+	if (concat_str == NULL)
+		return (NULL);
 
-    /* concatenate the strings */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            concat_str[k] = av[i][j];
-            k++;
-        }
-        concat_str[k] = '\n';
-        k++;
-    }
-    concat_str[k] = '\0';
+	/* concatenate the strings */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			concat_str[k] = av[i][j];
+			k++;
+		}
+		concat_str[k] = '\n';
+		k++;
+	}
+	concat_str[k] = '\0';
 
-    return (concat_str);
+	return (concat_str);
 }
