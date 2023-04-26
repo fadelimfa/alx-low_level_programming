6dcecd8799d92dcde6be38faa1f69880a128f1f0
fadelimfa <fadeli.mfa@gmail.com>
2023-04-26 09:36:24 -0700

    00

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,42 +6,27 @@
 
 
 /**
- * _calloc - allocates memory for an array
- * @nmemb: number of elements in the array
+ * _calloc - allocates memory for an array of nmemb elements of size bytes
+ * @nmemb: number of elements
  * @size: size of each element
  *
- * Return: a pointer to the allocated memory, or NULL if an error occurred
+ * Return: pointer to the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+    char *ptr;
+    unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return (NULL);
 
-	_memset(ptr, 0, nmemb * size);
+    /* Set memory to zero */
+    for (i = 0; i < nmemb * size; i++)
+        *(ptr + i) = 0;
 
-	return (ptr);
-}
-
-/**
- * _memset - fills memory with a constant byte
- * @s: pointer to the memory area to fill
- * @b: the constant byte to fill with
- * @n: number of bytes to fill
- *
- * Return: a pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+    return ((void *)ptr);
 }
