05170907e7a9525860b3b9e4c80349fdbd0cacbb
fadelimfa <fadeli.mfa@gmail.com>
2023-04-26 08:22:41 -0700

    sd

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,43 +3,48 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
- *str_concat - check the code.
- *@s1: is
- *@s2: is
- * Return: Always 0.
+ * **alloc_grid - is
+ * @height: is
+ * @width: i
+ * Return: 0 on success, 1 on failure
  */
 
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    if (width <= 0 || height <= 0) {
-        return NULL;
-    }
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * height);
 
-    grid = malloc(sizeof(int *) * height);
-    if (grid == NULL) {
-        return NULL;
-    }
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < height; i++) {
-        grid[i] = malloc(sizeof(int) * width);
-        if (grid[i] == NULL) {
-            /* Free previously allocated rows */
-            for (j = 0; j < i; j++) {
-                free(grid[j]);
-            }
-            free(grid);
-            return NULL;
-        }
-        for (j = 0; j < width; j++) {
-            grid[i][j] = 0;
-        }
-    }
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+		/* Free previously allocated rows */
+		for (j = 0; j < i; j++)
+		{
+			free(grid[j]);
+		}
+		free(grid);
+		return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
 
-    return grid;
+	return (grid);
 }
 
