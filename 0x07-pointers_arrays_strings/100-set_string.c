b610f874693cf50a32f0ce589ae52a6da7aa3c57
fadelimfa <fadeli.mfa@gmail.com>
2023-04-27 07:16:34 -0700

    d

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ *set_string - check the code.
+ *@s: is
+ *@to: is
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
