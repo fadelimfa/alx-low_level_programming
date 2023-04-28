13620d91b57b08cd0ccd5ce4b64a1aafc2978669
fadelimfa <fadeli.mfa@gmail.com>
2023-04-28 07:47:39 -0700

    sd

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,62 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-#define MAX_DIGITS 10000
-
-
-void validate_number(char *num) {
-    if (num == NULL || strlen(num) == 0) {
-        printf("Error: invalid input\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < (int)strlen(num); i++) {
-        if (num[i] < '0' || num[i] > '9') {
-            printf("Error: invalid input\n");
-            exit(1);
+int is_valid_number(char *s) {
+    for (int i = 0; i < strlen(s); i++) {
+        if (!isdigit(s[i])) {
+            return 0;
         }
     }
+    return 1;
 }
 
-char *multiply(char *num1, char *num2) {
-    validate_number(num1);
-    validate_number(num2);
-
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    char *result = (char *)calloc(len1 + len2 + 1, sizeof(char));
-
-    if (result == NULL) {
-        printf("Error: memory allocation failed\n");
-        exit(1);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Error\n");
+        return 98;
     }
-
-    for (int i = len1 - 1; i >= 0; i--) {
-        int carry = 0;
-        for (int j = len2 - 1; j >= 0; j--) {
-            int k = i + j + 1;
-            int temp = (num1[i] - '0') * (num2[j] - '0') + carry + result[k] - '0';
-            result[k] = temp % 10 + '0';
-            carry = temp / 10;
-        }
-        result[i] += carry;
+    if (!is_valid_number(argv[1]) || !is_valid_number(argv[2])) {
+        printf("Error\n");
+        return 98;
     }
-
-    if (result[0] == '0') {
-        memmove(result, &result[1], len1 + len2);
-    }
-
-    return result;
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    printf("%d\n", num1 * num2);
+    return 0;
 }
-
-int main(void) {
-    char num1[MAX_DIGITS];
-    char num2[MAX_DIGITS];
-    char result[MAX_DIGITS];
-
-    printf("Enter first number: ");
-    fgets(num1, MAX_DIGITS, stdin);
-    printf("Enter second number: ");
-    fgets(num2, MAX_DIGITS, stdin);
-
