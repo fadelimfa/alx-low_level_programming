bcf1d919aaab1da62b8230d96b150e7186e519ff
fadelimfa <fadeli.mfa@gmail.com>
2023-04-28 06:56:15 -0700

    sd

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+	int denominations[] = {25, 10, 5, 2, 1};
+	int num_denominations = sizeof(denominations) / sizeof(int);
+
+	if (argc != 2) {
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0) {
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 0; i < num_denominations; i++) {
+		coins += cents / denominations[i];
+		cents %= denominations[i];
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
+
