8fb92a524bb3eb98499a199a40c8ef69c8892822
fadelimfa <fadeli.mfa@gmail.com>
2023-04-28 03:45:57 -0700

    ds

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,30 +10,30 @@
  */
 int main(int argc, char **argv)
 {
-    int bytes, i;
+	int bytes, i;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-    if (bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    for (i = 0; i < bytes; i++)
-    {
-        printf("%02hhx", *((char *)main + i));
-        if (i != bytes - 1)
-            printf(" ");
-        else
-            printf("\n");
-    }
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i != bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
-    return (0);
+	return (0);
 }
