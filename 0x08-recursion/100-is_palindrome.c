9890a83225ff19a6a5b0ad4046de24816da9c44c
fadelimfa <fadeli.mfa@gmail.com>
2023-04-28 06:45:07 -0700

    ds

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,10 +8,10 @@
  */
 int find_strlen(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    else
-        return (1 + find_strlen(s + 1));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + find_strlen(s + 1));
 }
 
 /**
@@ -25,11 +25,11 @@ int find_strlen(char *s)
  */
 int check_palindrome(char *s, int start, int end)
 {
-    if (start >= end)
-        return (1);
-    if (s[start] != s[end])
-        return (0);
-    return (check_palindrome(s, start + 1, end - 1));
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check_palindrome(s, start + 1, end - 1));
 }
 
 /**
@@ -41,7 +41,7 @@ int check_palindrome(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = find_strlen(s);
-    return (check_palindrome(s, 0, len - 1));
+	int len = find_strlen(s);
+	return (check_palindrome(s, 0, len - 1));
 }
 
