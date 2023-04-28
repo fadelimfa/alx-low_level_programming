d47750d8d0311b125be72c5bd8c6219a4f43029a
fadelimfa <fadeli.mfa@gmail.com>
2023-04-28 07:11:36 -0700

    sd

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
+/**
+ * main - entry point for the program
+ * @argc: number of command-line arguments
+ * @argv: array of command-line argument strings
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
@@ -8,19 +16,22 @@ int main(int argc, char *argv[])
 	int num_denominations = sizeof(denominations) / sizeof(int);
 	int i;
 
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		printf("Error\n");
 		return (1);
 	}
 
 	cents = atoi(argv[1]);
 
-	if (cents < 0) {
+	if (cents < 0)
+	{
 		printf("0\n");
 		return (0);
 	}
 
-	for ( i = 0; i < num_denominations; i++) {
+	for (i = 0; i < num_denominations; i++)
+	{
 		coins += cents / denominations[i];
 		cents %= denominations[i];
 	}
