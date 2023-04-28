7d02c0bf3081c74f050f2dd814e6ced717bc64f4
fadelimfa <fadeli.mfa@gmail.com>
2023-04-28 06:49:42 -0700

    ds

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 
 /**
  * wildcmp - Compares two strings and returns 1 if they can be considered
- *           identical, otherwise returns 0. s2 can contain the special character
+ *      s2 can contain the special character
  *           * which can replace any string (including an empty string).
  *
  * @s1: The first string to be compared.
@@ -16,7 +16,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '\0')
 		return (*s1 == '\0');
 
-	if (*s2 == '*') {
+	if (*s2 == '*')
+       	{
 		if (*(s2 + 1) == '\0')
 			return (1);
 
