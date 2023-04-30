3932d9dda256e1f0de9d47e7e2ac2caf42ff6532
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 04:46:44 -0700

    sd

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,31 +12,35 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new_node, *current;
+	listint_t *newnode, *current = *head;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	if (idx == 0)
-		return (add_nodeint(head, n));
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
 
-	current = *head;
-	for (i = 0; i < idx - 1; i++)
+	if (idx == 0)
 	{
-		if (current == NULL)
-			return (NULL);
-			current = current->next;
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
 	}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+	if (current == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
+	newnode->next = current->next;
+	current->next = newnode;
 
-	return (new_node);
+	return (newnode);
 }
 
