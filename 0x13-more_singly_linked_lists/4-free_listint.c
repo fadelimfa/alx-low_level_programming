6cd6012da1ce3f277497efb9fa0ad4dcc1de4e3c
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 04:29:18 -0700

    sd

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to head of list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
