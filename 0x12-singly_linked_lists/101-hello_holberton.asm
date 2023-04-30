bc8e521c845e1bf281dccad5126339e83821742e
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 04:05:28 -0700

    as

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
 section .data
-    hello db "Hello, Holberton", 0Ah, 0
+    msg: db "Hello, Holberton", 10
 
 section .text
-    global _start
+    global main
+    extern printf
 
-_start:
-    ; write "Hello, Holberton" to stdout
-    push hello
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rdi, msg
+    mov rax, 0
     call printf
-    add rsp, 8
 
-    ; exit
-    xor eax, eax
-    mov ebx, eax
-    inc eax
-    int 0x80
-
+    mov eax, 0
+    leave
+    ret
