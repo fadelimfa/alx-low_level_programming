d1f5d1b7da7592afba8115b974e17bdb27fa1462
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 03:31:32 -0700

    sd

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
 #include "lists.h"
-void __attribute__((constructor)) before_main()
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
