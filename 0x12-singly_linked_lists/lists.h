0f6a620ec2cd596eb8c5433e8c6c60db94dd12ae
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 03:10:09 -0700

    ds

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -21,8 +21,45 @@ typedef struct list_s
 /* function prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+
+/* Define add_node_end here */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_node, *tmp_node;
+
+    if (!head || !str)
+        return (NULL);
+
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (!new_node->str)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = NULL;
+
+    if (!*head)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+
+    tmp_node = *head;
+    while (tmp_node->next)
+        tmp_node = tmp_node->next;
+
+    tmp_node->next = new_node;
+
+    return (new_node);
+}
+
 void free_list(list_t *head);
 
 #endif /* LISTS_H */
+
