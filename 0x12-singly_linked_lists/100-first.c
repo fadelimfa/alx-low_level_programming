3001958ed118f4cf94080f8b35f3cf8dbb116bd8
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 04:02:23 -0700

    ds

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,11 @@
 void print_before_main(void) __attribute__ ((constructor));
 
 /**
- * print_before_main - prints a message before the main function is executed
+ * print_before_main - prints a message bed
  */
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
 }
 
