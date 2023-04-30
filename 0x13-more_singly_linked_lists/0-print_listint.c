ab2e266ce1274a6f8c3be38568d9c4013d0e539d
fadelimfa <fadeli.mfa@gmail.com>
2023-04-30 04:15:52 -0700

    ds

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: Pointer to the head of the list.
+ * @h: Pointer to the head node of the list.
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
