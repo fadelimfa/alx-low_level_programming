5fdc97a7fc648107c5d0a88f8b653b0c32dc561f
fadelimfa <fadeli.mfa@gmail.com>
2023-11-23 13:41:46 -0800

    fixing malloc 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <limits.h> 
+#include <stdlib.h> 
 
 /**
  * malloc_checked - check the code.
@@ -17,5 +15,5 @@ void *malloc_checked(unsigned int b)
 	{
 		exit(98);
 	}
-	return (ptr);
+	return ptr;
 }
